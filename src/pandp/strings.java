724b5b350ad49e4d5be56e0c75f3db88aacd83f3
pulki <pulki@DESKTOP-R77BQ6G>
2018-07-14 10:44:01 +0530

    reversing word letters

--- src/pandp/strings.java
+++ src/pandp/strings.java
@@ -16,6 +16,22 @@ public class strings {
             }
         }
          System.out.println(str);
-	}
+	
+System.out.println("reverse");
+	String input = "Prerna Great";
+    char[] temparray = input.toCharArray();
+    int left, right=0;
+    right = temparray.length-1;
 
+    for (left=0; left < right ; left++ ,right--)
+    {
+        char temp = temparray[left];
+        temparray[left] = temparray[right];
+        temparray[right]=temp;
+    }
+
+    for (char c : temparray)
+        System.out.print(c);
+    System.out.println();
+}
 }
