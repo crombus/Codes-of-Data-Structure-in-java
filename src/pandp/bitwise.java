32cfe0f862c6169fa41b06d5aa54151fb7712e2e
pulki <pulki@DESKTOP-R77BQ6G>
2018-07-16 13:29:32 +0530

    prerna second method done

--- src/pandp/bitwise.java
+++ src/pandp/bitwise.java
@@ -17,10 +17,40 @@ public class bitwise {
 			return 0;
 		return (n%2==0?0:1) + setbituse(n/2);
 	}
+	 static int setbits2(int n)
+	    {
+	        int i = 0;
+	        int ans = 0;
+	        while ((1 << i) <= n) {
+	 
+	            boolean k = false;
+	            int change = 1 << i;
+	            for (int j = 0; j <= n; j++) {
+	 
+	                if (k == true)
+	                    ans += 1;
+	                else
+	                    ans += 0;
+	 
+	                if (change == 1) {
+	                    k = !k; 
+	                    change = 1 << i; 
+	                }
+	                else {
+	                    change--;
+	                }
+	            }
+	            i++;
+	        }
+	 
+	        return ans;
+	    }
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int n=3;
-System.out.println(setbit(n));
+System.out.println(setbit(n));//complexity(nlogn)
+System.out.println(setbits2(n));//complexity(k*n)
 	}
 
 }
