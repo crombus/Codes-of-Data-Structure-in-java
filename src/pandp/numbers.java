9ae8bea7ae1cbb97471f24298750481a36a13e4f
pulki <pulki@DESKTOP-R77BQ6G>
2018-07-16 18:54:59 +0530

    path for a matrix

--- src/pandp/numbers.java
+++ src/pandp/numbers.java
@@ -17,9 +17,18 @@ public class numbers {
 	       return 5;
 	    else return  conv0To5(num);
 	}
+	static int path(int m,int n)
+	{
+		if(m==1||n==1)
+		{
+			return 1;
+		}
+		return path(m-1,n)+path(m,n-1);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println(convert0to5(1032000706));
+System.out.println(convert0to5(Integer.parseInt(("1032000706"))));
+System.out.println(path(3,3));
 	}
 
 }
