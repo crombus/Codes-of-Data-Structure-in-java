5b66925dac462d864def5f00b6337f32327b3707
pulki <pulki@DESKTOP-R77BQ6G>
2018-07-24 10:37:13 +0530

    fill the water link in next commit

--- src/pandp/pattern.java
+++ src/pandp/pattern.java
@@ -5,6 +5,8 @@ public class pattern {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 pat(16);
+int a[]= {2,0,2};
+System.out.println(fillthewater(a));
 	}
 static void pat(int n)
 {
@@ -16,5 +18,27 @@ static void pat(int n)
 	System.out.println(n+" ");
 	pat(n-5);
 	System.out.println(n+" ");
+}
+static int fillthewater(int a[])
+{
+	int water=0;
+	int s=a.length;
+	int l[]=new int[s];
+	int r[]=new int[s];
+	l[0]=a[0];
+	for(int i=1;i<s;i++)
+	{
+		l[i]=Math.max(l[i-1], a[i]);
+	}
+	r[s-1]=a[s-1];
+	for(int i=s-2;i>=0;i--)
+	{
+		r[i]=Math.max(r[i+1], a[i]);
+	}
+	for(int i=0;i<s;i++)
+	{
+		water+=Math.min(l[i], r[i])-a[i];
+	}
+	return water;
 }
 }
