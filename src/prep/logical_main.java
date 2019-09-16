c775991ec1e07edf4f35cb48fe866dff6b6f7a97
crombus <pkundra@redhat.com>
2019-09-16 16:53:55 +0530

    Add code for removing duplicates from sorted array

--- src/prep/logical_main.java
+++ src/prep/logical_main.java
@@ -1,10 +1,13 @@
 import java.io.*;
 
 public class logical_main{
-
+//logical_main class has only main function 
     public static void main(String[] args)throws IOException{
-
-        array_ops obj = new array_ops(10);
+      // main function
+        array_ops obj = new array_ops(10);//creating obj for array operations 
         int[] array = obj.create_array();
+        remove_duplicates rd = new remove_duplicates();
+        rd._remove_duplicate(array);
         obj.display();
+        
     }}
---
+++ src/prep/remove_duplicates.java
@@ -0,0 +1,23 @@
+class remove_duplicates{
+
+    public int _remove_duplicate_sorted(int[] nums) {
+        //remove duplicates from sorted array 
+    /*Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+      Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+      Example 1:
+      Given nums = [1,1,2], Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+      It doesn't matter what you leave beyond the returned length.
+      link--> https://leetcode.com/problems/remove-duplicates-from-sorted-array/ */
+        int sum = 1, flag = 0;
+        for(int i = 0;i<nums.length - 1;i++){
+            int var = nums[i];
+            if(var != nums[i + 1]){
+                sum++;
+                flag++;
+                nums[flag] = nums[i + 1];
+            }
+        }
+        /*Runtime: 1 ms, faster than 96.84% of Java online submissions for Remove Duplicates from Sorted Array.
+          Memory Usage: 39.3 MB, less than 99.47% of Java online submissions for Remove Duplicates from Sorted Array.*/
+        return sum;
+    }}
